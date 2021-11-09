10aa3e0bbcd7c4f9d79cef619e31ce11751f0660
Bronagh Colton <61869605+Bocolo@users.noreply.github.com>
2021-11-09 10:27:01 +0000

    Working Exponent - test loop

--- Exponentiation.java
+++ Exponentiation.java
@@ -2,41 +2,52 @@ import java.util.*;
 public class Exponentiation {
     public static void main(String[] args) {
         Scanner myObj= new Scanner(System.in);
+
+        for(int i = 0; i<10;i++){
         System.out.print("Please input the number to be expo'd....: ");
         int x= myObj.nextInt();
         System.out.print("Please input the exponent...: ");
         int n= myObj.nextInt();
-        myObj.close();
-        System.out.println(ExponentThisMofo(x, n)+"...1");
-        System.out.println(Exponent(x, n)+"...2");
-        System.out.println(Exponent2(x, n)+"...3");
-    }
-//are both number ints or doubles ... ? ask in class
-    public static int ExponentThisMofo(int x, int n){
-        int y=x; 
-        for(int i =1 ; i< n;i++){
-            y*=x;
+
+        
+        System.out.println(Exponent(x, n)+"...");
         }
-        return y;
+        myObj.close();
     }
-    public static int Exponent(int z, int y){
-      if (y ==0){
+
+    public static double Exponent(double z, int y){
+      if(y<0){
+          double result5 = Exponent(1/z, -y);
+          return result5;
+      }
+        else if (y ==0){
           return 1;
       }else if(y%2==0){
-          int result2=((Exponent(z*z, y/2)));//*z);
+          double result2=((Exponent(z*z, y/2)));//*z);
           return result2;
       }else{
-          int result4 = (Exponent(z*z, (y-1)/2))*z;
+          double result4 = (Exponent(z*z, (y-1)/2))*z;
           return result4;
       }
       
     }
-    public static int Exponent2(int a, int b){
-        if (b ==0){
-            return 1;
-        }else{
-            int result3=((Exponent2(a*a, b/2)));
-            return result3;
-      }
+
 }
-}
+    // public static int Exponent2(int a, int b){
+    //     if (b ==0){
+    //         return 1;
+    //     }else{
+    //         int result3=((Exponent2(a*a, b/2)));
+    //         return result3;
+    //   }
+    // }
+    // System.out.println(Exponent2(x, n)+"...3");
+    // System.out.println(ExponentThisMofo(x, n)+"...1");
+    //are both number ints or doubles ... ? ask in class
+    // public static int ExponentThisMofo(int x, int n){
+    //     int y=x; 
+    //     for(int i =1 ; i< n;i++){
+    //         y*=x;
+    //     }
+    //     return y;
+    // }
