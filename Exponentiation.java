1c13bb064ff5a2943b9f53f1795dc5587f60b050
Bronagh Colton <61869605+Bocolo@users.noreply.github.com>
2021-11-09 02:25:28 +0000

    everything is weird , not doing as it should- too tired

--- Exponentiation.java
+++ Exponentiation.java
@@ -7,10 +7,11 @@ public class Exponentiation {
         System.out.print("Please input the exponent...: ");
         int n= myObj.nextInt();
         myObj.close();
-        System.out.println(ExponentThisMofo(x, n));
-
+        System.out.println(ExponentThisMofo(x, n)+"...1");
+        System.out.println(Exponent(x, n)+"...2");
+        System.out.println(Exponent2(x, n)+"...3");
     }
-
+//are both number ints or doubles ... ? ask in class
     public static int ExponentThisMofo(int x, int n){
         int y=x; 
         for(int i =1 ; i< n;i++){
@@ -18,4 +19,23 @@ public class Exponentiation {
         }
         return y;
     }
+    public static int Exponent(int x, int n){
+      if (n ==0){
+          return 1;
+      }else if(n%2==0){
+          int result=((Exponent(x*x, n/2))*x);
+          return result;
+      }else{
+          return 49;
+      }
+      
+    }
+    public static int Exponent2(int x, int n){
+        if (n ==0){
+            return 1;
+        }else{
+            int result=((Exponent(x*x, n/2)));
+            return result;
+      }
+}
 }
